948fa0ef20527ff512d185fb37d9d26ca363dde4
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-11 11:09:22 +0700

    feat: implement initial animation, double tap gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
   // MARK: Properties
 
   @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
 
   // MARK: Body
 
@@ -25,9 +26,27 @@ struct ContentView: View {
           .cornerRadius(10)
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .scaleEffect(imageScale)
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1
+              }
+            }
+          })
       } //: ZStack
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform: {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
+        }
+      })
     } //: NavigationView
     .navigationViewStyle(.stack)
   }
