9f7aaa2b33b8e8ab67da351d4b41cea1d284814c
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-11 12:38:58 +0700

    feat: implement drag gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -12,6 +12,16 @@ struct ContentView: View {
 
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
+
+  // MARK: Methods
+
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
 
   // MARK: Body
 
@@ -27,25 +37,42 @@ struct ContentView: View {
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1 : 0)
+          .animation(.linear(duration: 1), value: isAnimating)
+          .offset(x: imageOffset.width, y: imageOffset.height)
           .scaleEffect(imageScale)
+
+          // MARK: Double tap gesture
+
           .onTapGesture(count: 2, perform: {
             if imageScale == 1 {
               withAnimation(.spring()) {
                 imageScale = 5
               }
             } else {
-              withAnimation(.spring()) {
-                imageScale = 1
-              }
+              resetImageState()
             }
           })
+
+          // MARK: Drag gesture
+
+          .gesture(
+            DragGesture()
+              .onChanged { value in
+                withAnimation(.linear(duration: 0.5)) {
+                  imageOffset = value.translation
+                }
+              }
+              .onEnded { _ in
+                if imageScale <= 1 {
+                  resetImageState()
+                }
+              }
+          )
       } //: ZStack
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)
       .onAppear(perform: {
-        withAnimation(.linear(duration: 1)) {
-          isAnimating = true
-        }
+        isAnimating = true
       })
     } //: NavigationView
     .navigationViewStyle(.stack)
