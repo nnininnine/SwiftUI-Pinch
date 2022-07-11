b760f56adf45f20397dc74e6f2390e41ade7108c
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-11 09:35:42 +0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Nuttapon Buaban on 11/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
