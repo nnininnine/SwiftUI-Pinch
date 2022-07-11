f3f435d000601fdc6429c547ae2a2d0c256d827c
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-11 11:01:29 +0700

    feat: add magazine image, navigation view

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  // MARK: Properties
+
+  @State private var isAnimating: Bool = false
+
+  // MARK: Body
+
+  var body: some View {
+    NavigationView {
+      ZStack {
+        // MARK: Page Image
+
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+      } //: ZStack
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+    } //: NavigationView
+    .navigationViewStyle(.stack)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
